e8c9c08a47cf7605977ca1163dc4f44da7910dee
Laura Bustamante <91298068+lbte@users.noreply.github.com>
2022-11-12 20:19:26 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -14,6 +14,19 @@ import numpy as np
 
 def ingest_data():
   df = pd.read_fwf('./clusters_report.txt', colspecs="infer", widths=[7, 11, 13, 300])
+  with open('./clusters_report.txt', 'r') as cluster_file:
+    splitted_lines = [line.split(", ") for line in cluster_file.readlines()]
+
+    cols11 = splitted_lines[0][0].split()
+    cols12 = splitted_lines[1][0].split()
+    col1 = cols11[0]
+    col2 = ' '.join(cols11[1:3])
+    col3 = ' '.join(cols11[3:5])
+    col4 = ' '.join(cols11[5:]) 
+    col2 += ' ' + ' '.join(cols12[0:2])
+    col3 += ' ' + ' '.join(cols12[2:])
+
+    columnas = [col1, col2, col3, col4]
   df.columns = columnas
   df = df.iloc[2:, :].reset_index()
   df.drop("index", inplace=True, axis='columns')
