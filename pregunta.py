ea9c3472eeb6b15bdbc10aab38d62c4a14c8412e
Laura Bustamante <91298068+lbte@users.noreply.github.com>
2022-11-12 20:15:15 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -17,13 +17,18 @@ def ingest_data():
   df.columns = columnas
   df = df.iloc[2:, :].reset_index()
   df.drop("index", inplace=True, axis='columns')
+
   df.iloc[:,:-1].fillna(method = "ffill", inplace = True)
   df[df.columns[0]] = df[df.columns[0]].astype(int)
   non_last = df.columns[:-1]
-  df_corrected = df.groupby(df.columns[0])["Principales palabras clave"].apply(lambda x: ' '.join(x))
+  df_corrected = df.groupby(df.columns[0])["Principales palabras clave"].apply(lambda x: ', '.join(x))
   df_corrected[df.columns[0]] = np.arange(1, df.iloc[:, 0].max())
   df = df.merge(df_corrected, on=df.columns[0], how="left").drop_duplicates(non_last)
   df.drop("Principales palabras clave_x", inplace=True, axis=1)
   df.rename(columns={'Principales palabras clave_y':"Principales palabras clave"}, inplace=True)
+  cols = list(map(lambda x: x.lower().replace(" ", "_"), df.columns))
+  cols = {original:renamed for original, renamed in zip(df.columns, cols)}
+  df.rename(columns = cols, inplace = True)
+  
 
   return df
